33b589ffb9719ac6067a36c4eb68613e9ca97a12
Kundan <kd.cod.1198@gmail.com>
2026-03-05 14:45:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-//required modules //
 const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const router = express.Router();
+const path = require("path");
+
 const port = process.env.PORT || 5000;
 
-
-//to serve static files  such as js,css,images//
 app.use(express.static(path.join(__dirname, "public")));
 
-
-//to render main page//
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-
-
-// Listen on Port 5000
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
